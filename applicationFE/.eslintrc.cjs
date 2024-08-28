5ad92586c1670b3b14612cf29aac8d0fc09c4262
ysh <ysh0208@strato.co.kr>
2024-08-28 16:40:47 +0900

    UI Template 작업

---
+++ applicationFE/.eslintrc.cjs
@@ -0,0 +1,19 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  'extends': [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier/skip-formatting'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    // should always be multi-word 적용 X
+    'vue/multi-word-component-names' : 0
+  },
+}
