65217df80711fd9afbfd60d8bcc498dcecf12c62
pbccc <pbccc@strato.co.kr>
2024-08-28 09:15:33 +0900

    repository edit

---
+++ src/main/java/kr/co/mcmp/util/FileUtil.java
@@ -0,0 +1,49 @@
+package kr.co.mcmp.util;
+
+import kr.co.mcmp.dto.oss.component.CommonUploadComponent;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItem;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+public class FileUtil {
+
+    public static MultipartFile generatedMultipartFile(CommonUploadComponent.TextComponentDto textComponent) throws IOException {
+        byte[] content = textComponent.getText().getBytes();
+        String fileName = textComponent.getFilename() + "." + textComponent.getExtension();
+        String mimeType = getMimeType(textComponent.getExtension());
+
+        FileItem fileItem = new DiskFileItem(
+                "file",
+                mimeType,
+                false,
+                fileName,
+                content.length,
+                null
+        );
+
+        try (ByteArrayInputStream inputStream = new ByteArrayInputStream(content)) {
+            fileItem.getOutputStream().write(inputStream.readAllBytes());
+        }
+
+        return new CommonsMultipartFile(fileItem);
+    }
+
+    private static String getMimeType(String extension) {
+        switch (extension.toLowerCase()) {
+            case "txt":
+                return "text/plain";
+            case "html":
+                return "text/html";
+            case "sh":
+                return "application/x-sh";
+            case "yaml":
+                return "text/x-yaml";
+            default:
+                return "application/octet-stream";
+        }
+    }
+}
