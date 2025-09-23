37009a9027796a7bd4b03cad5e4131fe1a2b6f2f
hyun123 <hyun123@stratro.co.kr>
2025-09-23 21:08:10 +0900

    update.

--- src/main/java/kr/co/mcmp/externalrepo/DockerHubInterface.java
+++ src/main/java/kr/co/mcmp/externalrepo/DockerHubInterface.java
@@ -17,8 +17,8 @@ public interface DockerHubInterface {
     //https://hub.docker.com/v2/namespaces/{keyword}/repositories
 
     @GetMapping(value="/api/search/v3/catalog/search")
-    DockerHubCatalog searchCatalog(@RequestParam("query") String keyword,
-                                  @RequestParam(required=false, value="badges", defaultValue="official") String badges);
+    DockerHubCatalog searchCatalog(@RequestParam("query") String keyword)
+                                //   @RequestParam(required=false, value="badges", defaultValue="official") String badges);
     //https://hub.docker.com/api/search/v3/catalog/search?query=ubuntu&badges=official
 
 
